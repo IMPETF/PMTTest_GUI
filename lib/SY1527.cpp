431e696512f04be0c78547c7cd95f758c58d24bf
Yong <yong@impcas.ac.cn>
2014-09-30 08:44:56 +0800

    edit

--- lib/SY1527.cpp
+++ lib/SY1527.cpp
@@ -1,5 +1,164 @@
 #include "SY1527.h"
 
-SY1527::SY1527()
+using namespace std;
+
+SY1527::SY1527():
+    fStatus(CAENHV_OK)
 {
+
+}
+
+SY1527::SY1527(const char *IP, const char *usrName, const char *pssWord):
+    fIPAddr(IP),fUserName(usrName),fPassWord(pssWord),handle(0),fSatus(CAENHV_OK)
+{
+
+}
+
+SY1527::~SY1527()
+{
+
+}
+
+bool SY1527::status()
+{
+    if(fStatus == CAENHV_OK)
+        return true;
+    else
+        return false;
+}
+
+string SY1527::getErrorDesc()
+{
+    return fErrorDesc;
+}
+
+bool SY1527::connect()
+{
+    fStatus=CAENHV_InitSystem(SY1527,LINKTYPE_TCPIP,fIPAddr.c_str(),
+                              fUserName.c_str(),fPassWord.c_str(),&fHandle);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc="Can't connect SY1527";
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::disConnect()
+{
+    fStatus=CAENHV_DeinitSystem(fHandle);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc="Can't disconnect SY1527";
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::setChName(unsigned short slot, unsigned short chnum, const unsigned short *chlist, const char *chname)
+{
+    fStatus=CAENHV_SetChName(fHandle,slot,chnum,chlist,chname);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::setChName(unsigned short slot, unsigned short ch_id, const char *chname)
+{
+    fStatus=CAENHV_SetChName(fHandle,slot,1,&ch_id,chname);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::setChName(const HVChannels &channels)
+{
+    HVChannels::iterator it;
+    for(it=channels.begin();it!=channels.end();it++){
+        if(!setChName(*it))
+            return false;
+    }
+    return true;
+}
+
+bool SY1527::setChName(const HVChannel &channel)
+{
+    fStatus=CAENHV_SetChName(fHandle,channel.slot,1,&(channel.ch_id),channel.ch_name);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::getChName(unsigned short slot, unsigned short ch_num, const unsigned short *chlist, char (*chname)[])
+{
+    fStatus=CAENHV_GetChName(fHandle,slot,ch_num,chlist,chname);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::getChName(unsigned short slot, unsigned short ch_id, char *chname)
+{
+    return getChName(slot,1,&ch_id,chname);
+}
+
+bool SY1527::getChName(HVChannel &channel)
+{
+    return getChName(channel.slot,channel.ch_id,channel.ch_name);
+}
+
+bool SY1527::getChName(HVChannels &channels)
+{
+    HVChannels::iterator it;
+    for(it=channels.begin();it!=channels.end();it++){
+        if(!getChName(*it))
+            return false;
+    }
+    return true;
+}
+
+bool SY1527::setChParam(unsigned short slot, const char *param, unsigned short chnum, const unsigned short *chlist, void *parvalue)
+{
+    fStatus=CAENHV_SetChParam(fHandle,slot,param,chnum,chlist,parvalue);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::getChParam(unsigned short slot, const char *param, unsigned short chnum, const unsigned short *chlist, void *parvallist)
+{
+    fStatus=CAENHV_GetChParam(fHandle,slot,param,chnum,chlist,parvallist);
+    if(fStatus != CAENHV_OK){
+        fErrorDesc=CAENHV_GetError(fHandle);
+        return false;
+    }
+    else
+        return true;
+}
+
+bool SY1527::TurnOn(unsigned short slot, unsigned short chnum, const unsigned short *chlist)
+{
+    unsigned long Pw=1;
+    return setChParam(slot,"Pw",chnum,chlist,&Pw);
+}
+
+bool SY1527::TurnOff(unsigned short slot, unsigned short chnum, const unsigned short *chlist)
+{
+    unsigned long Pw=0;
+    return setChParam(slot,"Pw",chnum,chlist,&Pw);
 }
--- lib/SY1527.h
+++ lib/SY1527.h
@@ -7,13 +7,14 @@
 
 struct HVChannel
 {
-    int slot;
+    ushort slot;
     ushort ch_id;
     char ch_name[MAX_CH_NAME];
     float V0Set;
     float I0Set;
     float VMon;
     float IMon;
+    bool state;
 };
 typedef std::vector<HVChannel> HVChannels;
 typedef std::map<int,HVChannels> HVGroup;
@@ -21,7 +22,7 @@ typedef std::map<int,HVChannels> HVGroup;
 class SYX527_Module
 {
 public:
-    SYX527_Module(int slot,HVChannels& group);
+    SYX527_Module(SY1527* controller,int slot,HVChannels& channels);
     ~SYX527_Module();
 
 public:
@@ -29,12 +30,18 @@ public:
     inline void setISet(float iset) {fISet=iset;}
     inline void setfRUp(float rup) {fRUp=rup;}
     inline void setfRDWn(float rdown) {fRDWn=rdown;}
+    inline ushort getSlot() const {return slot;}
 
 private:
-    int slot;
+    SY1527* crate;
+    ushort slot;
     ushort fChNum;
-    ushort *fChList;
-    std::string *fChSetName;
+    //ushort *fChList;
+    std::vector<ushort> fChList;
+    ushort* fPChList;
+    //std::string *fChSetName;
+    std::vector<string> fChSetName;
+    std::string *fPChSetName;
 
     //input
     float fVSet;
@@ -44,11 +51,19 @@ private:
     float fRDWn;
 
     //output
-    float *fV0Set;
-    float *fI0Set;
-    float *fVMon;
-    float *fIMon;
-    char (*fChName)[MAX_CH_NAME];
+    //float *fV0Set;
+    std::vector<float> fV0Set;
+    float* fPV0Set;
+    //float *fI0Set;
+    std::vector<float> fI0Set;
+    float* fPI0Set;
+    //float *fVMon;
+    std::vector<float> fVMon;
+    float* fPVMon;
+    //float *fIMon;
+    std::vector<float> fIMon;
+    float* fPIMon;
+    char (*fPChName)[MAX_CH_NAME];
 
 };
 
@@ -56,6 +71,53 @@ class SY1527
 {
 public:
     SY1527();
+    SY1527(const char* IP,const char* usrName,const char* pssWord);
+    ~SY1527();
+
+public:
+    inline void setIP(const char* ipAddr) {fIPAddr=ipAddr;}
+    inline void setUsername(const char* name) {fUserName=name;}
+    inline void setPassword(const char* psswd) {fPassWord=psswd;}
+    inline std::string getIP() {return fIPAddr;}
+    inline std::string getUsername() {return fUserName;}
+    inline std::string getPassword() {return fPassWord;}
+    //status
+    bool status();
+    std::string getErrorDesc();
+    //conncetion
+    bool connect();
+    bool disConnect();
+    //utility
+    bool setChName(ushort slot,ushort ch_id,const char* chname);
+    bool setChName(ushort slot,ushort chnum,const ushort *chlist,const char* chname);
+    bool setChName(const HVChannel& channel);
+    bool setChName(const HVChannels& channels);
+    bool getChName(ushort slot,ushort ch_id,char* chname);
+    bool getChName(ushort slot,ushort ch_num,const ushort *chlist,char (*chname)[MAX_CH_NAME]);
+    bool getChName(HVChannel& channel);
+    bool getChName(HVChannels& channels);//channels must be in the same slot
+
+    bool setChParam(ushort slot,const char* param,ushort chnum,const ushort *chlist,void* parvalue);
+    bool getChParam(ushort slot,const char* param,ushort chnum,const ushort *chlist,void* parvallist);
+
+    //usful functions
+    bool TurnOn(ushort slot,ushort chnum,const ushort *chlist);
+    bool TurnOff(ushort slot,ushort chnum,const ushort *chlist);
+    bool SetRampUp(ushort slot,ushort chnum,const ushort *chlist,float RUp);
+    bool SetRampDown(ushort slot,ushort chnum,const ushort *chlist,float RDwn);
+    bool SetV0Set(ushort slot,ushort chnum,const ushort *chlist,float V);
+    bool SetI0Set(ushort slot,ushort chnum,const ushort *chlist,float I);
+
+private:
+    //config
+    std::string fIPAddr;
+    std::string fUserName;
+    std::string fPassWord;
+    int fHandle;
+    //status
+    CAENHVRESULT fStatus;
+    std::string fErrorDesc;
+
 };
 
 #endif // SY1527_H
